1030e47371c6dd60deab0a695fbb5b25b2de55c9
ManavYagnik <98301029+ManavYagnik@users.noreply.github.com>
2023-07-27 03:35:39 +0530

    some changes in css

--- src/components/Website.js
+++ src/components/Website.js
@@ -3,26 +3,47 @@ import React, { Fragment } from 'react'
 function Website() {
   return (
     <Fragment>
-    <div className='website-container'>
-        qhgoqhgoqhgoho
-        qgoqjgoqg
-        qgqgqegqge
+    <div className='website-container-1'>
+
+        <div className='website-container-1-text'>
+
+        </div>
+
+        <div className='website-container-1-image'>
+            
+            </div>
+       
       
     </div>
 
-    <div className='website-container'>
-        qhgoqhgoqhgoho
-        qgoqjgoqg
-        qgqgqegqge
+
+
+
+
+
+      <div className='website-container-2'>
+
+      <div className='website-container-2-image'>
+            
+            </div> 
+       
+
+        <div className='website-container-2-text'>
+
+        </div>
+
+       
       
     </div>
 
-    <div className='website-container'>
-        qhgoqhgoqhgoho
-        qgoqjgoqg
-        qgqgqegqge
-      
-    </div>
+
+
+
+
+
+
+
+
 
    
     </Fragment>
